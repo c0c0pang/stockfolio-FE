d6834e9094677826bfad84d409dfbc098084517b
c0c0pang <coc1789@naver.com>
2026-01-28 14:41:13 +0900

    feat: 컴포넌트 인덱스 파일 추가

---
+++ src/global/components/index.ts
@@ -0,0 +1,22 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { Card } from './Card';
+export { Loading } from './Loading';
+export { Onboarding } from './Onboarding';
+export { ScreenContainer } from './ScreenContainer';
+export { ThemeToggle } from './ThemeToggle';
+export { ThemeTransition } from './ThemeTransition';
+export { Backdrop } from './Backdrop';
+export { Modal } from './Modal';
+export { Drawer } from './Drawer';
+export {
+  Skeleton,
+  TotalAssetCardSkeleton,
+  ExchangeRateCardSkeleton,
+  ProfitRateCardSkeleton,
+  AllocationChartSkeleton,
+  StockCardSkeleton,
+  NewsCardSkeleton,
+  TermCardSkeleton,
+  StockDetailSkeleton,
+} from './Skeleton';
